f98466b6aa86c675e6979bf7cbd14145c47a7806
ClaytonTaylor <claytaylor@cox.net>
2016-07-08 11:03:08 -0600

    JS-ProblemSet1 Complete

---
+++ JS-Problem-Set1.js
@@ -0,0 +1,27 @@
+// #1
+
+/*
+
+function letterCapitalize(string) {
+	var splitStr = string.toLowerCase().split(' ');
+   for (var i = 0; i < splitStr.length; i++) {
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+   return splitStr.join(' ');
+}
+
+letterCapitalize("hello, my name is clay.")
+
+*/
+
+// #2
+
+/*
+
+function wordCount(string) { 
+  return string.split(" ").length;
+}
+
+console.log(wordCount("Hello World my name is Clay Taylor."));
+
+*/
